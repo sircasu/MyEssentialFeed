c3cb2b39511bd061c5a369c18b551cd2b1c67b31
Matteo Casu <matteo.casu91@gmail.com>
2023-10-05 08:32:48 +0200

    Create the FeedItem model

---
+++ MyEssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  MyEssentialFeed
+//
+//  Created by Matteo Casu on 05/10/23.
+//
+
+import Foundation
+
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
