180cdd0f26f7133634a56aa838476b55278b3335
Matteo Casu <matteo.casu91@gmail.com>
2023-10-05 08:27:26 +0200

    Initial Commit

---
+++ MyEssentialFeed/MyEssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  MyEssentialFeed.h
+//  MyEssentialFeed
+//
+//  Created by Matteo Casu on 05/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyEssentialFeed.
+FOUNDATION_EXPORT double MyEssentialFeedVersionNumber;
+
+//! Project version string for MyEssentialFeed.
+FOUNDATION_EXPORT const unsigned char MyEssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyEssentialFeed/PublicHeader.h>
+
+
