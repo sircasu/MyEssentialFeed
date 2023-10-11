4864395faaf0ab37f438b90dc3514ce9e90ae3b2
Matteo Casu <matteo.casu91@gmail.com>
2023-10-11 05:13:11 +0200

    Creates URLSession data task with URL on `URLSessionHTTPClient`

---
+++ MyEssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,53 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  MyEssentialFeedTests
+//
+//  Created by Matteo Casu on 11/10/23.
+//
+
+import XCTest
+
+
+class URLSessionHTTPClient {
+    
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+    func test_getFromURL_createsDataTaskWithURL() {
+        
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    private class URLSessionSpy: URLSession {
+        
+        var receivedURLs = [URL]()
+        
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+}
