be3fd00f59835b73ecf6609184b2564eafe3de1e
Matteo Casu <matteo.casu91@gmail.com>
2023-10-14 17:32:52 +0200

    Extract test setup to a helper method

--- MyEssentialFeedAPIEndToEndTests/MyEssentialFeedAPIEndToEndTests.swift
+++ MyEssentialFeedAPIEndToEndTests/MyEssentialFeedAPIEndToEndTests.swift
@@ -12,22 +12,9 @@ final class MyEssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
         
-        let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerUrl, client: client)
-        
-        let exp = expectation(description: "Wait for load completion")
-        
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 5.0)
         
         
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items):
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             
@@ -53,7 +40,26 @@ final class MyEssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
+    private func getFeedResult() -> LoadFeedResult? {
+        
+        let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerUrl, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
+    }
+    
     private func expectedItem(at index: Int) -> FeedItem {
+        
         return FeedItem(
             id: id(at: index),
             description: description(at: index),
