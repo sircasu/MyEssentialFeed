4040973fdd7fddfcdbf76b5c04a861739f45bace
Matteo Casu <matteo.casu91@gmail.com>
2023-10-24 17:34:28 +0200

    Save command requests cache deletion

--- MyEssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ MyEssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,19 +6,29 @@
 //
 
 import XCTest
-
+import MyEssentialFeed
 
 class LocalFeedLoader {
     
-    let store: FeedStore
+    private let store: FeedStore
+    
     init(store: FeedStore) {
         self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     
     var deleteCachedFeedCallCount = 0
+    
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 
@@ -27,8 +37,31 @@ final class CacheFeedUseCaseTests: XCTestCase {
     func test_doesNotDeleteCacheUponCreation() {
         
         let store = FeedStore()
-        
         _ = LocalFeedLoader(store: store)
+        
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    
+    func test_save_requestCacheDeletion() {
+        
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    
+    // MARK: - Helpers
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "a desc", location: "a loc", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
 }
