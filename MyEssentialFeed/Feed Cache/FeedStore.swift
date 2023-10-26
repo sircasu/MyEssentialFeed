b80788d4ee83c700b1854d1ebd50b7dfb6e0f9ed
Matteo Casu <matteo.casu91@gmail.com>
2023-10-26 15:04:29 +0200

    Move `FeedStore` to its own file

---
+++ MyEssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  MyEssentialFeed
+//
+//  Created by Matteo Casu on 26/10/23.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
