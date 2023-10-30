7af6860ab19b8896873b0082c90d2ee9b77d754a
Matteo Casu <matteo.casu91@gmail.com>
2023-10-30 18:30:07 +0100

    Load command delivers cached images on less than seven days old cache

--- MyEssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ MyEssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -39,13 +39,21 @@ public final class LocalFeedLoader {
     
     public func load(completion: @escaping (LoadResult) -> Void) {
         
-        store.retrieve { error in
+        store.retrieve { result in
             
-            if let error = error {
+            switch result {
+            case let .failure(error):
                 completion(.failure(error))
-            } else {
+            
+            case let .found(feed, _):
+                completion(.success(feed.toModels()))
+            
+            
+            case .empty:
                 completion(.success([]))
             }
+            
+
         
         }
     }
@@ -69,6 +77,15 @@ private extension Array where Element == FeedImage {
     func toLocal() -> [LocalFeedImage] {
         return map {
             LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
+        }
+    }
+}
+
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map {
+            FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url)
         }
     }
 }
--- MyEssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
+++ MyEssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTest.swift
@@ -50,6 +50,19 @@ class LoadFeedFromCacheUseCaseTest: XCTestCase {
     }
     
     
+    
+    func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+                
+        expect(sut, toCompleteWith: .success(feed.models), when: {
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        })
+    }
+    
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -88,7 +101,43 @@ class LoadFeedFromCacheUseCaseTest: XCTestCase {
     }
     
     
+    private func uniqueImage() -> FeedImage {
+        return FeedImage(id: UUID(), description: "a desc", location: "a loc", url: anyURL())
+    }
+    
+
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        
+        let items = [uniqueImage(), uniqueImage()]
+        let localItems = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+        
+        return (items, localItems)
+    }
+    
+    
+    
     private func anyNSError() -> NSError {
         return NSError(domain: "any error", code: 0)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
+}
+
+
+
+private extension Date {
+    
+    func adding(days: Int) -> Date {
+        
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        
+        return self + seconds
     }
 }
