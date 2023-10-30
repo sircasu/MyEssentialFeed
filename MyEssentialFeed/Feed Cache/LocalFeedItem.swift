ffb42a1f2d23bf731fab000d250169b10779e4a5
Matteo Casu <matteo.casu91@gmail.com>
2023-10-30 10:40:40 +0100

    Move `LocalFeedItem` to its own file

---
+++ MyEssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,23 @@
+//
+//  LocalFeedItem.swift
+//  MyEssentialFeed
+//
+//  Created by Matteo Casu on 30/10/23.
+//
+
+import Foundation
+
+// This is our Feed Item representation for the Cache Module` - DTO data transfer object - data transfer of the model to remove strong coupling between modules
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
