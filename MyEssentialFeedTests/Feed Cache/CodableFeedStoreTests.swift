36d2384e1d23390231743d0a30c18b3668c6fbe2
Matteo Casu <matteo.casu91@gmail.com>
2023-11-04 23:30:43 +0100

    Extract duplicate delete test code into reusable helper method

--- MyEssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ MyEssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -205,15 +205,10 @@ class CodableFeedStoreTests: XCTestCase {
     func test_delete_hasNoSideEffectsOnEmptyCache() {
         
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache deletion")
+        
+        let deletionError = deleteCache(from: sut)
 
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-        
+        XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
         expect(sut, toRetrieve: .empty)
     }
     
@@ -223,14 +218,9 @@ class CodableFeedStoreTests: XCTestCase {
         let sut = makeSUT()
         insert(cache: (uniqueImageFeed().local, Date()), to: sut)
         
-        let exp = expectation(description: "Wait for cache deletion")
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
-        
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
         expect(sut, toRetrieve: .empty)
     }
     
@@ -256,6 +246,20 @@ class CodableFeedStoreTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         return insertionError
+    }
+    
+    
+    private func deleteCache(from sut: CodableFeedStore) -> Error? {
+        let exp = expectation(description: "Wait for cache deletion")
+        var deletionError: Error?
+        sut.deleteCachedFeed { receivedDeletionError in
+            deletionError = receivedDeletionError
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        return deletionError
     }
     
     
