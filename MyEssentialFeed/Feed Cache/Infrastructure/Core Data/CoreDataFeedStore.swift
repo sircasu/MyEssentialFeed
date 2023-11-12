ac88c003988659e87dd2a645dab93e54e8c2f709
Matteo Casu <matteo.casu91@gmail.com>
2023-11-12 17:19:37 +0100

    MOve `CoreDataFeedStore` files to the new infrastructure folder

--- MyEssentialFeed/Feed Cache/Infrastructure/Core Data/CoreDataFeedStore.swift
+++ MyEssentialFeed/Feed Cache/Infrastructure/Core Data/CoreDataFeedStore.swift
@@ -44,8 +44,7 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         
-        let context = self.context
-        context.perform {
+       perform { context in
             
             do {
                 
@@ -65,8 +64,7 @@ public final class CoreDataFeedStore: FeedStore {
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
-        let context = self.context
-        context.perform {
+        perform { context in
     
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
@@ -76,6 +74,13 @@ public final class CoreDataFeedStore: FeedStore {
             }
         }
     
+    }
+    
+    
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
     }
     
 }
