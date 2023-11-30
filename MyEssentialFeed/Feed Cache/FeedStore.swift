5275c7d5eab26546773a45707ffab6009efcb882
Matteo Casu <matteo.casu91@gmail.com>
2023-11-30 08:36:17 +0100

    Add typealias for `FeedStore.DeletionResult` and `FeedStore.InsertionResult`

--- MyEssentialFeed/Feed Cache/FeedStore.swift
+++ MyEssentialFeed/Feed Cache/FeedStore.swift
@@ -40,7 +40,10 @@ public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 
 public protocol FeedStore {
+    typealias DeletionResult = Error?
     typealias DeletionCompletion = (Error?) -> Void
+    
+    typealias InsertionResult = Error?
     typealias InsertionCompletion = (Error?) -> Void
     
     typealias RetrievalResult = Result <CachedFeed?, Error>
