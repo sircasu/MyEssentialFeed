b71be36d89d1d675363e1d5656427ca143528270
Matteo Casu <matteo.casu91@gmail.com>
2023-12-08 23:33:04 +0100

    Render loaded feed on successful load completion

---
+++ MyEssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  MyEssentialFeediOS
+//
+//  Created by Matteo Casu on 08/12/23.
+//
+
+import UIKit
+
+
+public final class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
--- MyEssentialFeediOS/FeedViewController.swift
+++ MyEssentialFeediOS/FeedViewController.swift
@@ -13,6 +13,7 @@ final public class FeedViewController: UITableViewController {
     private var onViewIsAppearing: ((FeedViewController) -> Void)?
     
     private var loader: FeedLoader?
+    private var tableModel = [FeedImage]()
     
     // convenience initializer becase we don't need any custom initialization (in this way we don't need to implement UIViewController's required initializer)
     public convenience init(loader: FeedLoader) {
@@ -40,9 +41,26 @@ final public class FeedViewController: UITableViewController {
     
     @objc private func load() {
         refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
+        loader?.load { [weak self] result in
+            self?.tableModel = (try? result.get()) ?? []
+            self?.tableView.reloadData()
             self?.refreshControl?.endRefreshing()
         }
 
+    }
+    
+    
+    public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableModel.count
+    }
+    
+    public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellModel = tableModel[indexPath.row]
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (cellModel.location == nil)
+        cell.locationLabel.text = cellModel.location
+        cell.descriptionLabel.text = cellModel.description
+        return cell
     }
 }
