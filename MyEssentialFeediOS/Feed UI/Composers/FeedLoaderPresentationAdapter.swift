3c1eb2ff7f5ccf9bc821adda2add4929248bcdae
Matteo Casu <matteo.casu91@gmail.com>
2024-01-03 08:37:33 +0100

    Move `FeedLoaderPresentationAdapter` to a separate file

---
+++ MyEssentialFeediOS/Feed UI/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,35 @@
+//
+//  FeedLoaderPresentationAdapter.swift
+//  MyEssentialFeediOS
+//
+//  Created by Matteo Casu on 03/01/24.
+//
+
+import MyEssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+
+    
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    func didRequestFeedRefresh() {
+        
+        presenter?.didStartLoadingFeed()
+        
+        feedLoader.load { [weak self] result in
+            
+            switch result {
+            case let .success(feed):
+                self?.presenter?.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter?.didFinishLoadingFeed(with: error)
+            }
+        }
+        
+    }
+}
