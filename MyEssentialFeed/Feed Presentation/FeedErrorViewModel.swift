b38531d907074da01c4926bec1ef0be151ca16a0
Matteo Casu <matteo.casu91@gmail.com>
2024-01-12 19:50:08 +0100

    Move `FeedViewModel` to separate file

---
+++ MyEssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FeedErrorViewModel.swift
+//  MyEssentialFeed
+//
+//  Created by Matteo Casu on 12/01/24.
+//
+
+import Foundation
---
+++ MyEssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  MyEssentialFeed
+//
+//  Created by Matteo Casu on 12/01/24.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
