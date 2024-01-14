85fd7dba1f99efc52791eaf947156e4398aa214e
Matteo Casu <matteo.casu91@gmail.com>
2024-01-14 08:15:08 +0100

    Extract duplicate base URL creation into a factory helper

--- MyEssentialFeedAPIEndToEndTests/MyEssentialFeedAPIEndToEndTests.swift
+++ MyEssentialFeedAPIEndToEndTests/MyEssentialFeedAPIEndToEndTests.swift
@@ -54,10 +54,9 @@ final class MyEssentialFeedAPIEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
         
-        let testServerUrl = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedLoader(url: testServerUrl, client: client)
-        
+        let loader = RemoteFeedLoader(url: feedTestServerURL, client: client)
+
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         
@@ -75,7 +74,8 @@ final class MyEssentialFeedAPIEndToEndTests: XCTestCase {
     
     
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeaks(client, file: file, line: line)
@@ -84,7 +84,7 @@ final class MyEssentialFeedAPIEndToEndTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
 
         var receivedResult: FeedImageDataLoader.Result?
-        _ = loader.loadImageData(from: testServerURL) { result in
+        _ = loader.loadImageData(from: url) { result in
             receivedResult = result
             exp.fulfill()
         }
@@ -93,6 +93,11 @@ final class MyEssentialFeedAPIEndToEndTests: XCTestCase {
         return receivedResult
     }
     
+    
+    private var feedTestServerURL: URL {
+        return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+    }
+
     
     private func expectedImage(at index: Int) -> FeedImage {
         
