251286b09be70b017dd585f6fa9fb84040a28cd8
Matteo Casu <matteo.casu91@gmail.com>
2024-01-16 19:54:01 +0100

    Move `CoreDataFeedStore` implementation of the FeedStore protocol to separate file

--- MyEssentialFeed/Feed Cache/Infrastructure/Core Data/CoreDataFeedStore.swift
+++ MyEssentialFeed/Feed Cache/Infrastructure/Core Data/CoreDataFeedStore.swift
@@ -8,7 +8,7 @@
 import Foundation
 import CoreData
 
-public final class CoreDataFeedStore: FeedStore {
+public final class CoreDataFeedStore {
     
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
@@ -16,50 +16,6 @@ public final class CoreDataFeedStore: FeedStore {
     public init(storeURL: URL, bundle: Bundle = .main) throws {
         container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
         context = container.newBackgroundContext()
-    }
-    
-    
-    public func retrieve(completion: @escaping RetrievalCompletion) {
-        
-        perform { context in
-            
-            completion(Result(catching: {
-                
-                try ManagedCache.find(in: context).map {
-                    return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
-                }
-            }))
-        }
-    }
-
-    
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
-       perform { context in
-            
-           completion(Result {
-                
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-
-                managedCache.feed = ManagedFeedImage.image(from: feed, in: context)
-                
-                try context.save()
-            })
-        }
-    }
-    
-
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        
-        perform { context in
-    
-            completion(Result {
-
-                try ManagedCache.find(in: context).map(context.delete).map(context.save)
-            })
-        }
-    
     }
     
     
