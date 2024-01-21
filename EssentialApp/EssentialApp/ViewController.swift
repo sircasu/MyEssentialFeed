44235ff1e9d3d156ddc41670a88f6ac50807f7f7
Matteo Casu <matteo.casu91@gmail.com>
2024-01-21 18:10:37 +0100

    Add Empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Matteo Casu on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
