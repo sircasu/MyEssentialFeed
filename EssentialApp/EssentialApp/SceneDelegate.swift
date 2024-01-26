e8c7ad3d4c223ef5803276a6b36194b76cdebc79
Matteo Casu <matteo.casu91@gmail.com>
2024-01-26 22:09:02 +0100

    Display remote feed on launch when customer has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -17,23 +17,26 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
 //    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 //        
-//        guard let _ = (scene as? UIWindowScene) else { return }
-//        
-//        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-//        
-//        let session = URLSession(configuration: .ephemeral)
-//        let client = URLSessionHTTPClient(session: session)
-//        let feedLoader = RemoteFeedLoader(url: url, client: client)
-//        let imageLoader = RemoteFeedImageDataLoader(client: client)
-//        
-//        let feedViewController = FeedUIComposer.feedComposedWith(
-//            feedLoader: feedLoader,
-//            imageLoader: imageLoader)
-//        
-//        window?.rootViewController = feedViewController
+
 //    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+
+        let session = URLSession(configuration: .ephemeral)
+        
+        let remoteClient = URLSessionHTTPClient(session: session)
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+
+        window?.rootViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageLoader)
+        
         
 //        guard let _ = (scene as? UIWindowScene) else { return }
 //        
