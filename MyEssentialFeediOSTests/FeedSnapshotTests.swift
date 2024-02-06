420e1b7d9c4a1da4b63e34046a430526238c78be
Matteo Casu <matteo.casu91@gmail.com>
2024-02-06 17:40:45 +0100

    Record feed with failed image loading snapshot

--- MyEssentialFeediOSTests/FeedSnapshotTests.swift
+++ MyEssentialFeediOSTests/FeedSnapshotTests.swift
@@ -39,6 +39,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT() -> FeedViewController {
@@ -49,25 +57,40 @@ class FeedSnapshotTests: XCTestCase {
         return controller
     }
     
-    public func emptyFeed() -> [FeedImageCellController] {
+    private func emptyFeed() -> [FeedImageCellController] {
         return []
     }
     
     private func feedWithContent() -> [ImageStub] {
-            return [
-                ImageStub(
-                    description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
-                    location: "East Side Gallery\nMemorial in Berlin, Germany",
-                    image: UIImage.make(withColor: .red)
-                ),
-                ImageStub(
-                    description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
-                    location: "Garth Pier",
-                    image: UIImage.make(withColor: .green)
-                )
-            ]
-        }
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
     
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: nil,
+                location: "Cannon Street, London",
+                image: nil
+            ),
+            ImageStub(
+                description: nil,
+                location: "Brighton Seafront",
+                image: nil
+            ),
+        ]
+    }
     
 
     private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
