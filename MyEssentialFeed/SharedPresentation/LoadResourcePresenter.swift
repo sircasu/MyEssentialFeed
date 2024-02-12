114b843f724c98bd951f6c1463268e2438ac8fda
Matteo Casu <matteo.casu91@gmail.com>
2024-02-12 10:44:07 +0100

    Make `LoadResourcePresenter` generic over `Resource` types

--- MyEssentialFeed/SharedPresentation/LoadResourcePresenter.swift
+++ MyEssentialFeed/SharedPresentation/LoadResourcePresenter.swift
@@ -8,14 +8,15 @@
 import Foundation
 
 public protocol ResourceView {
-    func display(_ viewModel: String)
+    associatedtype ResourceViewModel
+    func display(_ viewModel: ResourceViewModel)
 }
 
-public final class LoadResourcePresenter {
+public final class LoadResourcePresenter<Resource, View: ResourceView> {
     
-    public typealias Mapper = (String) -> (String)
+    public typealias Mapper = (Resource) -> View.ResourceViewModel
     
-    private let resourceView: ResourceView
+    private let resourceView: View
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
     private let mapper: Mapper
@@ -27,7 +28,7 @@ public final class LoadResourcePresenter {
                                  comment: "Error message displayed when we can't load the image feed from the server")
     }
     
-    public init(resourceView: ResourceView, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
+    public init(resourceView: View, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
         self.resourceView = resourceView
         self.loadingView = loadingView
         self.errorView = errorView
@@ -43,7 +44,7 @@ public final class LoadResourcePresenter {
     
 
     
-    public func didFinishLoading(with resource: String) {
+    public func didFinishLoading(with resource: Resource) {
         
         resourceView.display(mapper(resource))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
--- MyEssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ MyEssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -61,12 +61,13 @@ final class LoadResourcePresenterTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    private typealias SUT = LoadResourcePresenter<String, ViewSpy>
     
     private func makeSUT(
-        mapper: @escaping LoadResourcePresenter.Mapper = { _ in "any" },
+        mapper: @escaping SUT.Mapper = { _ in "any" },
         file: StaticString = #file,
         line: UInt = #line
-    ) -> (sut: LoadResourcePresenter, view: ViewSpy) {
+    ) -> (sut: SUT, view: ViewSpy) {
         
         let view = ViewSpy()
         let sut = LoadResourcePresenter(resourceView: view, loadingView: view, errorView: view, mapper: mapper)
@@ -78,7 +79,7 @@ final class LoadResourcePresenterTests: XCTestCase {
     
     func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
         let table = "Feed"
-        let bundle = Bundle(for: LoadResourcePresenter.self)
+        let bundle = Bundle(for: SUT.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
             XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
@@ -87,6 +88,7 @@ final class LoadResourcePresenterTests: XCTestCase {
     }
     
     private class ViewSpy: ResourceView, FeedLoadingView, FeedErrorView {
+        typealias ResourceViewModel = String
         
         enum Message: Hashable {
             case display(errorMessage: String?)
