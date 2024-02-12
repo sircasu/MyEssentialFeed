556632c09b88c2c2369bf8eb210994e4d992aed4
Matteo Casu <matteo.casu91@gmail.com>
2024-02-12 17:13:21 +0100

    Replace `FeedPresenter` with generic presenter

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -9,7 +9,7 @@ import UIKit
 import MyEssentialFeed
 import MyEssentialFeediOS
 
-final class FeedViewAdapter: FeedView {
+final class FeedViewAdapter: ResourceView {
     
     private weak var controller: FeedViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
