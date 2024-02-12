9cfdcd3b08a7157145714dca6409fb50e7f1e8d6
Matteo Casu <matteo.casu91@gmail.com>
2024-02-12 17:41:54 +0100

    Make Presentation Adapter so it can be reused

---
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -0,0 +1,43 @@
+//
+//  LoadResourcePresentationAdapter.swift
+//  MyEssentialFeediOS
+//
+//  Created by Matteo Casu on 03/01/24.
+//
+import Combine
+import MyEssentialFeed
+import MyEssentialFeediOS
+
+final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
+    
+    private let loader: () -> AnyPublisher<Resource, Error>
+    private var cancellable: Cancellable?
+    var presenter: LoadResourcePresenter<Resource, View>?
+    
+    init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
+        self.loader = loader
+    }
+    
+    func loadResource() {
+        presenter?.didStartLoading()
+        
+        cancellable = loader().sink(
+            receiveCompletion: { [weak self] completion in
+                switch completion {
+                case .finished: break
+                    
+                case let .failure(error):
+                    self?.presenter?.didFinishLoading(with: error)
+                }
+            }, receiveValue: { [weak self] resource in
+                self?.presenter?.didFinishLoading(with: resource)
+            })
+    }
+}
+
+extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
+    
+    func didRequestFeedRefresh() {
+        loadResource()
+    }
+}
