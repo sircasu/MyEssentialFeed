75b9b29ae1f2c8f314a0e1b787bac6f7f238fa95
Matteo Casu <matteo.casu91@gmail.com>
2024-02-12 11:25:14 +0100

    Remove duplication in the localization test

--- MyEssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
+++ MyEssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
@@ -14,21 +14,9 @@ final class FeedLocalizationTests: XCTestCase {
     
     func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
         let table = "Feed"
-        let presentationBundle = Bundle(for: FeedPresenter.self)
-        let localizationBundles = allLocalizationBundles(in: presentationBundle)
-        let localizedStringKeys = allLocalizedStringKeys(in: localizationBundles, table: table)
-
-        localizationBundles.forEach { (bundle, localization) in
-            localizedStringKeys.forEach { key in
-                let localizedString = bundle.localizedString(forKey: key, value: nil, table: table)
-
-                if localizedString == key {
-                    let language = Locale.current.localizedString(forLanguageCode: localization) ?? ""
-
-                    XCTFail("Missing \(language) (\(localization)) localized string for key: '\(key)' in table: '\(table)'")
-                }
-            }
-        }
+        let bundle = Bundle(for: FeedPresenter.self)
+        
+        assertLocalizedKeyAndValueExist(in: bundle, table)
     }
     
     
