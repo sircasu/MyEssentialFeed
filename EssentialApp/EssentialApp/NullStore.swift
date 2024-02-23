e2c30c4a34b7e403fbdb95c98c6def989718a7ec
Matteo Casu <matteo.casu91@gmail.com>
2024-02-23 09:33:46 +0100

    Use `NullStore` when we can't create a `CoreDataFeedStore` instance - Add `assertionFailure` to catch CoreData issues in debug builds

---
+++ EssentialApp/EssentialApp/NullStore.swift
@@ -0,0 +1,34 @@
+//
+//  NullStore.swift
+//  EssentialApp
+//
+//  Created by Matteo Casu on 23/02/24.
+//
+
+import Foundation
+import MyEssentialFeed
+
+class NullStore: FeedStore & FeedImageDataStore {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        completion(.success(()))
+    }
+    
+    func insert(_ feed: [MyEssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        completion(.success(()))
+    }
+    
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.success(.none))
+
+    }
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        completion(.success(()))
+    }
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+    
+    
+}
